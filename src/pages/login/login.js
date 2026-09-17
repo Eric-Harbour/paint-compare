b223a3ae1dc2aa6346c73160771bd1a29fa0ace7
Eric Harbour <harbour_eric@outlook.com>
2026-09-17 01:46:49 -0500

    working on functionality of login page

---
+++ src/pages/login/login.js
@@ -0,0 +1,6 @@
+function signIn() {
+    var emailElement, passwordElement, email, password;
+    email = document.getElementById("email-input").value;
+    password = document.getElementById("password-input").value;
+    
+}
